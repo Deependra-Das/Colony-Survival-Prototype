13dfd67b4338d3663fe01cfcc0cc9ec4c783e8f6
Deependra-Das <iamdeep75@gmail.com>
2026-08-07 17:53:04 +0530

    Added PopulationData, ConsumptionData for mapping the Config Data

---
+++ Assets/Scripts/Colony/DataMap/PopulationData.cs
@@ -0,0 +1,10 @@
+namespace ColonySurvivalPrototype.Colony
+{
+    [System.Serializable]
+    public class PopulationData
+    {
+        public int villagersCount;
+        public float foodReserve;
+        public float waterReserve;
+    }
+}
