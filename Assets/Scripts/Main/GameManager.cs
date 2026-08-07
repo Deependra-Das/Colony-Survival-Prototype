994d079a10355b229ec784b1353bdea8149a1434
Deependra-Das <iamdeep75@gmail.com>
2026-08-07 12:48:24 +0530

    Added GameManager script

---
+++ Assets/Scripts/Main/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace ColonySurvivalPrototype.Main
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            Debug.Log("GameManager");
+        }
+    }
+}
