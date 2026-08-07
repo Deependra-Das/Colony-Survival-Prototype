7a2b703a63555267738224c8a4352f826aa91d5c
Deependra-Das <iamdeep75@gmail.com>
2026-08-07 13:52:12 +0530

    Updated ColonyUIManager to use game object references and display sample values for population, food & water text

--- Assets/Scripts/UI/ColonyUIManager.cs
+++ Assets/Scripts/UI/ColonyUIManager.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
+using TMPro;
 
 namespace ColonySurvivalPrototype.UI
 {
     public class ColonyUIManager : MonoBehaviour
     {
+        [SerializeField] private TMP_Text _populationText;
+        [SerializeField] private TMP_Text _foodText;
+        [SerializeField] private TMP_Text _waterText;
 
+        public static ColonyUIManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void Initialize()
+        {
+            _populationText.text = "0000";
+            _foodText.text = "0000";
+            _waterText.text = "0000";
+        }
     }
 }
