cc4910df7f3ca02fb0c433a249e14a9aee37b627
Deependra-Das <iamdeep75@gmail.com>
2026-08-07 18:02:00 +0530

    Added ColonyManager script & updated GameplayScene

---
+++ Assets/Scripts/Colony/ColonyManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using ColonySurvivalPrototype.Utility;
+
+namespace ColonySurvivalPrototype.Colony
+{
+    public class ColonyManager : MonoBehaviour
+    {
+        public static ColonyManager Instance { get; private set; }
+        private ColonySimulationService _colonySimulationServiceObj;
+        private JsonDataLoaderService _jsonLoaderServiceObj;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void Initialize(JsonDataLoaderService jsonLoaderService, ColonySimulationService colonySimulationService)
+        {
+            _jsonLoaderServiceObj = jsonLoaderService;
+            _colonySimulationServiceObj = colonySimulationService;
+
+            PopulationData populationData = _jsonLoaderServiceObj.Load<PopulationData>("population.json");
+            ConsumptionData consumptionData = _jsonLoaderServiceObj.Load<ConsumptionData>("consumption.json");
+
+            if (populationData != null && consumptionData != null)
+            {
+                _colonySimulationServiceObj.AddNewColony(populationData, consumptionData);
+                Debug.Log("New Colony Added");
+            }
+            else
+            {
+                Debug.Log("No Config Data Loaded");
+            }               
+        }
+    }
+}
