546340eab7a754d5601118eb44c45763c720bb16
Deependra-Das <iamdeep75@gmail.com>
2026-08-08 10:33:22 +0530

    Updated ColonyManager to add SimulationClock coroutine & related changes

--- Assets/Scripts/Colony/ColonyManager.cs
+++ Assets/Scripts/Colony/ColonyManager.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using System.Collections;
 using ColonySurvivalPrototype.Utility;
 
 namespace ColonySurvivalPrototype.Colony
 {
     public class ColonyManager : MonoBehaviour
     {
+        [SerializeField] private float dayDuration = 1f;
+
         public static ColonyManager Instance { get; private set; }
+
         private ColonySimulationService _colonySimulationServiceObj;
         private JsonDataLoaderService _jsonLoaderServiceObj;
+        private Coroutine _simulationClockCoroutine;
 
         private void Awake()
         {
@@ -35,11 +40,37 @@ namespace ColonySurvivalPrototype.Colony
                 ColonyData colonyData = _colonySimulationServiceObj.GetColonyDataByColonyId(0);
                 Debug.Log($"ColonyId: {colonyData.ColonyId}, VillagerCount: {colonyData.VillagersCount}, Food: {colonyData.FoodReserve}, Water: {colonyData.WaterReserve}," +
                     $"Food Consumption Per Villager Per Day: {colonyData.FoodConsumptionPerVillagerPerDay}, Water Consumption Per Villager Per Day: {colonyData.WaterConsumptionPerVillagerPerDay}, Day: {colonyData.Day}  ");
+
+                StartSimulationClock();
             }
             else
             {
                 Debug.Log("No Config Data Loaded");
             }               
+        }
+
+        private void StartSimulationClock()
+        {
+            _simulationClockCoroutine = StartCoroutine(SimulationClock());
+        }
+
+        private IEnumerator SimulationClock()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(dayDuration);
+
+                _colonySimulationServiceObj.AdvanceOneDayForVillageById(0);
+            }
+        }
+
+        private void StopSimulationClock()
+        {
+            if(_simulationClockCoroutine!=null)
+            {
+                StopCoroutine(_simulationClockCoroutine);
+                _simulationClockCoroutine = null;
+            }
         }
     }
 }
