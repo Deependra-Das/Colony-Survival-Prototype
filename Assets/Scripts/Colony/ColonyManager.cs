f35bef84c7f6bd70a94e7be32f73292f7bf23dca
Deependra-Das <iamdeep75@gmail.com>
2026-08-08 11:10:13 +0530

    Changes to use the SimulationClockButtonClickedEvent to start the SimulationClock coroutine

--- Assets/Scripts/Colony/ColonyManager.cs
+++ Assets/Scripts/Colony/ColonyManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using ColonySurvivalPrototype.Utility;
+using ColonySurvivalPrototype.Event;
 
 namespace ColonySurvivalPrototype.Colony
 {
@@ -12,7 +13,20 @@ namespace ColonySurvivalPrototype.Colony
 
         private ColonySimulationService _colonySimulationServiceObj;
         private JsonDataLoaderService _jsonLoaderServiceObj;
+        private EventBusService _eventBusServiceObj;
         private Coroutine _simulationClockCoroutine;
+        PopulationData populationData;
+        ConsumptionData consumptionData;
+
+        private void SubscribeToEvents()
+        {
+            _eventBusServiceObj.Subscribe<SimulationClockButtonClickedEvent>(StartSimulationClock);
+        }
+
+        private void UnsubscribeToEvents()
+        {
+            _eventBusServiceObj.Unsubscribe<SimulationClockButtonClickedEvent>(StartSimulationClock);
+        }
 
         private void Awake()
         {
@@ -26,30 +40,39 @@ namespace ColonySurvivalPrototype.Colony
             DontDestroyOnLoad(gameObject);
         }
 
-        public void Initialize(JsonDataLoaderService jsonLoaderService, ColonySimulationService colonySimulationService)
+        public void Initialize(JsonDataLoaderService jsonLoaderService, ColonySimulationService colonySimulationService, EventBusService eventBusService)
         {
             _jsonLoaderServiceObj = jsonLoaderService;
             _colonySimulationServiceObj = colonySimulationService;
+            _eventBusServiceObj= eventBusService;
 
-            PopulationData populationData = _jsonLoaderServiceObj.Load<PopulationData>("population.json");
-            ConsumptionData consumptionData = _jsonLoaderServiceObj.Load<ConsumptionData>("consumption.json");
+            SubscribeToEvents();
+            LoadVillageConfigData();
+            AddNewColony();
+        }
 
+        private void LoadVillageConfigData()
+        {
+            populationData = _jsonLoaderServiceObj.Load<PopulationData>("population.json");
+            consumptionData = _jsonLoaderServiceObj.Load<ConsumptionData>("consumption.json");
+        }
+
+        private void AddNewColony()
+        {
             if (populationData != null && consumptionData != null)
             {
                 _colonySimulationServiceObj.AddNewColony(populationData, consumptionData);
                 ColonyData colonyData = _colonySimulationServiceObj.GetColonyDataByColonyId(0);
                 Debug.Log($"ColonyId: {colonyData.ColonyId}, VillagerCount: {colonyData.VillagersCount}, Food: {colonyData.FoodReserve}, Water: {colonyData.WaterReserve}," +
                     $"Food Consumption Per Villager Per Day: {colonyData.FoodConsumptionPerVillagerPerDay}, Water Consumption Per Villager Per Day: {colonyData.WaterConsumptionPerVillagerPerDay}, Day: {colonyData.Day}  ");
-
-                StartSimulationClock();
             }
             else
             {
                 Debug.Log("No Config Data Loaded");
-            }               
+            }
         }
 
-        private void StartSimulationClock()
+        private void StartSimulationClock(SimulationClockButtonClickedEvent eventObj)
         {
             _simulationClockCoroutine = StartCoroutine(SimulationClock());
         }
@@ -71,6 +94,11 @@ namespace ColonySurvivalPrototype.Colony
                 StopCoroutine(_simulationClockCoroutine);
                 _simulationClockCoroutine = null;
             }
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribeToEvents();
         }
     }
 }
