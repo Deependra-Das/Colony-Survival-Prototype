343871cd64638a55ab33b3764dc43769eb12dc0c
Deependra-Das <iamdeep75@gmail.com>
2026-08-08 15:36:09 +0530

    Removed unused Log from ColonyManager

--- Assets/Scripts/Colony/ColonyManager.cs
+++ Assets/Scripts/Colony/ColonyManager.cs
@@ -63,8 +63,6 @@ namespace ColonySurvivalPrototype.Colony
             {
                 _colonySimulationServiceObj.AddNewColony(populationData, consumptionData);
                 ColonyData colonyData = _colonySimulationServiceObj.GetColonyDataByColonyId(0);
-                Debug.Log($"ColonyId: {colonyData.ColonyId}, VillagerCount: {colonyData.VillagersCount}, Food: {colonyData.FoodReserve}, Water: {colonyData.WaterReserve}," +
-                    $"Food Consumption Per Villager Per Day: {colonyData.FoodConsumptionPerVillagerPerDay}, Water Consumption Per Villager Per Day: {colonyData.WaterConsumptionPerVillagerPerDay}, Day: {colonyData.Day}  ");
             }
             else
             {
