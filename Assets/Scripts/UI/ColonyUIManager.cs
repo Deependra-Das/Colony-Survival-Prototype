f8120f3231ad2bfac3eff3713cb117f6e52677cd
Deependra-Das <iamdeep75@gmail.com>
2026-08-08 11:09:25 +0530

    Updated ColonyUIManager to add SimulationClockButton & raise event

--- Assets/Scripts/UI/ColonyUIManager.cs
+++ Assets/Scripts/UI/ColonyUIManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using ColonySurvivalPrototype.Main;
+using ColonySurvivalPrototype.Event;
 
 namespace ColonySurvivalPrototype.UI
 {
@@ -8,8 +11,24 @@ namespace ColonySurvivalPrototype.UI
         [SerializeField] private TMP_Text _populationText;
         [SerializeField] private TMP_Text _foodText;
         [SerializeField] private TMP_Text _waterText;
+        [SerializeField] private Button _simulationClockButton;
 
         public static ColonyUIManager Instance { get; private set; }
+
+        private EventBusService _eventBusServiceObj;
+
+        private void OnEnable() => SubscribeToEvents();
+        private void OnDisable() => UnsubscribeToEvents();
+
+        private void SubscribeToEvents()
+        {
+            _simulationClockButton.onClick.AddListener(OnSimulationClockButtonClicked);
+        }
+
+        private void UnsubscribeToEvents()
+        {
+            _simulationClockButton.onClick.RemoveListener(OnSimulationClockButtonClicked);
+        }
 
         private void Awake()
         {
@@ -25,9 +44,16 @@ namespace ColonySurvivalPrototype.UI
 
         public void Initialize()
         {
+            _eventBusServiceObj = GameManager.Instance.Services.Get<EventBusService>();
+
             _populationText.text = "0000";
             _foodText.text = "0000";
             _waterText.text = "0000";
+        }
+
+        private void OnSimulationClockButtonClicked()
+        {
+            _eventBusServiceObj.Publish(new SimulationClockButtonClickedEvent());
         }
     }
 }
