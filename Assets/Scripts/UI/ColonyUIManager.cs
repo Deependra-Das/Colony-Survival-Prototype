9cf18cd6c69ccc282215c283874cb46030d6011a
Deependra-Das <iamdeep75@gmail.com>
2026-08-08 12:28:59 +0530

    Updated ColonyUIManager for creating simulationbutton using event & set default & formated values on UI

--- Assets/Scripts/UI/ColonyUIManager.cs
+++ Assets/Scripts/UI/ColonyUIManager.cs
@@ -1,8 +1,8 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
-using ColonySurvivalPrototype.Main;
 using ColonySurvivalPrototype.Event;
+using ColonySurvivalPrototype.Main;
 
 namespace ColonySurvivalPrototype.UI
 {
@@ -11,23 +11,23 @@ namespace ColonySurvivalPrototype.UI
         [SerializeField] private TMP_Text _populationText;
         [SerializeField] private TMP_Text _foodText;
         [SerializeField] private TMP_Text _waterText;
-        [SerializeField] private Button _simulationClockButton;
+        [SerializeField] private Button _simulationClockButtonPrefab;
+        [SerializeField] private Transform _simulationButtonContainer;
 
         public static ColonyUIManager Instance { get; private set; }
 
         private EventBusService _eventBusServiceObj;
-
-        private void OnEnable() => SubscribeToEvents();
-        private void OnDisable() => UnsubscribeToEvents();
+        private const float defaultNumValue = 0f;
 
         private void SubscribeToEvents()
         {
-            _simulationClockButton.onClick.AddListener(OnSimulationClockButtonClicked);
+            _eventBusServiceObj.Subscribe<NewColonyAddedEvent>(OnNewColonyAddedEvent_UI);
+    
         }
 
         private void UnsubscribeToEvents()
         {
-            _simulationClockButton.onClick.RemoveListener(OnSimulationClockButtonClicked);
+            _eventBusServiceObj.Unsubscribe<NewColonyAddedEvent>(OnNewColonyAddedEvent_UI);
         }
 
         private void Awake()
@@ -45,15 +45,66 @@ namespace ColonySurvivalPrototype.UI
         public void Initialize()
         {
             _eventBusServiceObj = GameManager.Instance.Services.Get<EventBusService>();
-
-            _populationText.text = "0000";
-            _foodText.text = "0000";
-            _waterText.text = "0000";
+            SubscribeToEvents();
+            SetDefaultValuesOnUI();
         }
 
-        private void OnSimulationClockButtonClicked()
+        private void OnNewColonyAddedEvent_UI(NewColonyAddedEvent eventObj)
         {
-            _eventBusServiceObj.Publish(new SimulationClockButtonClickedEvent());
+            CreateSimulationButton(eventObj.ColonyId);
+        }
+
+        private void CreateSimulationButton(int colonyId)
+        {
+            var button = Instantiate(_simulationClockButtonPrefab, _simulationButtonContainer);
+
+            button.onClick.AddListener(() => HandleButtonClicked(colonyId));
+        }
+
+        private void HandleButtonClicked(int colonyId)
+        {
+            Debug.Log($"ButtonClicked {colonyId}");
+        }
+
+        private void SetDefaultValuesOnUI()
+        {
+            UpdateFoodText(defaultNumValue);
+            UpdateWaterText(defaultNumValue);
+            UpdatePopulationText((int)defaultNumValue);
+        }
+
+        private void UpdateFoodText(float value)
+        {
+            _foodText.text = FormatNumber(value);
+        }
+
+        private void UpdateWaterText(float value)
+        {
+            _waterText.text = FormatNumber(value);
+        }
+
+        private void UpdatePopulationText(int value)
+        {
+            _populationText.text = FormatNumber(value);
+        }
+
+        private string FormatNumber(float value)
+        {
+            if (value < 10_000)
+                return value.ToString();
+
+            if (value < 1_000_000)
+                return $"{value / 1000f:0.#}K";
+
+            if (value < 1_000_000_000)
+                return $"{value / 1_000_000f:0.#}M";
+
+            return $"{value / 1_000_000_000f:0.#}B";
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribeToEvents();
         }
     }
 }
