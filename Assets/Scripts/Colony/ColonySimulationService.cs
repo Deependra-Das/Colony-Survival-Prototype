5bcd479153a4a322fbcee14b87e711dc77126dac
Deependra-Das <iamdeep75@gmail.com>
2026-08-08 12:29:46 +0530

    Updated ColonySimulationService to raise NewColonyAddedEvent

--- Assets/Scripts/Colony/ColonySimulationService.cs
+++ Assets/Scripts/Colony/ColonySimulationService.cs
@@ -1,23 +1,29 @@
 using UnityEngine;
 using System.Collections.Generic;
+using ColonySurvivalPrototype.Event;
 
 namespace ColonySurvivalPrototype.Colony
 {
     public class ColonySimulationService
     {
         private Dictionary<int, ColonyData> _colonyDataDictionary;
+        private EventBusService _eventBusServiceObj;
 
         public int ColonyCount => _colonyDataDictionary.Count;
 
-        public ColonySimulationService()
+        public ColonySimulationService(EventBusService eventBusService)
         {
-            _colonyDataDictionary = new Dictionary<int, ColonyData>();  
+            _eventBusServiceObj = eventBusService;
+            _colonyDataDictionary = new Dictionary<int, ColonyData>();
         }
 
         public void AddNewColony(PopulationData populationData, ConsumptionData consumptionData)
         {
             int newColonyId = ColonyCount;
-            _colonyDataDictionary.Add(newColonyId, new ColonyData(newColonyId, populationData, consumptionData));
+            ColonyData newColony = new ColonyData(newColonyId, populationData, consumptionData);
+            _colonyDataDictionary.Add(newColonyId, newColony);
+            RaiseNewColonyAddedEvent(newColony.ColonyId);
+            Debug.Log($"ColonyId: {newColony.ColonyId}, VillagerCount: {newColony.VillagersCount}, Food: {newColony.FoodReserve}, Water: {newColony.WaterReserve}, Day: {newColony.Day}");
         }
 
         public ColonyData GetColonyDataByColonyId(int ColonyId)
@@ -37,6 +43,10 @@ namespace ColonySurvivalPrototype.Colony
             colony.AdvanceToNextDay(foodUsed, waterUsed);
 
             Debug.Log($"ColonyId: {colony.ColonyId}, VillagerCount: {colony.VillagersCount}, Food: {colony.FoodReserve}, Water: {colony.WaterReserve}, Day: {colony.Day}");
+        }
+        private void RaiseNewColonyAddedEvent(int ColonyId)
+        {
+            _eventBusServiceObj.Publish(new NewColonyAddedEvent(ColonyId));
         }
     }
 }
